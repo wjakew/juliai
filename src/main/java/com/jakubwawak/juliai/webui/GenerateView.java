f1d818ffce8e24da06bacd1f0855db07ef7450c2
Jakub Wawak <34348257+wjakew@users.noreply.github.com>
2023-12-26 09:49:40 +0100

    jai261223REV01

--- src/main/java/com/jakubwawak/juliai/webui/GenerateView.java
+++ src/main/java/com/jakubwawak/juliai/webui/GenerateView.java
@@ -7,18 +7,23 @@ package com.jakubwawak.juliai.webui;
 
 import com.jakubwawak.juliai.JuliaiApplication;
 import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H6;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.theme.lumo.Lumo;
+import org.apache.commons.compress.archivers.sevenz.CLI;
 import org.json.JSONObject;
+import org.springframework.scheduling.annotation.Async;
 
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -70,7 +75,7 @@ public class GenerateView extends VerticalLayout {
         generate_area = new TextArea("Ask me anything...");
         generate_area.setSizeFull();
         generate_area.setPlaceholder("type your prompt here");
-        generate_button = new Button("Generate!", VaadinIcon.ACADEMY_CAP.create(),this::setGenerate_button);
+        generate_button = new Button("Generate!", VaadinIcon.ACADEMY_CAP.create(),this::setGenerate_button2);
         generate_button.setWidth("50%");
         generate_button.addThemeVariants(ButtonVariant.LUMO_CONTRAST,ButtonVariant.LUMO_PRIMARY);
 
@@ -86,48 +91,85 @@ public class GenerateView extends VerticalLayout {
         add(generate_layout);
         add(generate_button);
     }
-    //--button action functions
 
-    /**
-     * generate_button action
-     * @param ex
-     */
+    //--button action functions
+    @Async
     private void setGenerate_button(ClickEvent ex){
-        String prompt = generate_area.getValue();
-        if ( !prompt.isEmpty() ){
-            generate_area.setReadOnly(true);
-            generate_area.setTitle(generate_area.getValue());
+        String prompt = generate_area.getValue();// Lock the Vaadin session for thread-safe access
+        UI ui = UI.getCurrent();
+        if (!prompt.isEmpty()) {
+            generate_area.setValue(prompt); // Set the initial prompt text
+
+            // Create a separate thread to update the TextArea's content
+            Thread updateThread = new Thread(() -> {
+                StringBuilder data = new StringBuilder();
+
+                try {
+                    HttpURLConnection connection = (HttpURLConnection) new URL(JuliaiApplication.oacs.requestUrl + "/api/generate").openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setRequestProperty("Content-Type", "application/json");
+                    connection.setDoOutput(true);
+
+                    connection.getOutputStream().write(JuliaiApplication.oacs.prepareRequestPayload("llama2", prompt).getBytes());
+
+                    Scanner scanner = new Scanner(new InputStreamReader(connection.getInputStream()));
+
+                    while (scanner.hasNextLine()) {
+                        String line = scanner.nextLine();
+                        JSONObject jsonObject = new JSONObject(line);
+                        data.append(jsonObject.getString("response")); // Accumulate the response in the string builder
+
+                        // Update the text area asynchronously on the main UI thre
+                        ui.access(()->{
+                            generate_area.setValue(data.toString());
+                        });
+
+                        System.out.println(data.toString());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+
+            try{
+                VaadinSession.getCurrent().lock();
+                updateThread.start();
+            }finally {
+                // Lock the Vaadin session for thread-safe access
+                VaadinSession.getCurrent().unlock();
+            }
+        } else {
+            Notification.show("Prompt is empty!");
+        }
+    }
+
+    private void setGenerate_button2(ClickEvent ex){
+        String prompt = generate_area.getValue();// Lock the Vaadin session for thread-safe access
+        if (!prompt.isEmpty()) {
             StringBuilder data = new StringBuilder();
+            generate_area.setValue(data.toString());
             try {
-                HttpURLConnection connection = (HttpURLConnection) new URL(JuliaiApplication.oacs.requestUrl+"/api/generate").openConnection();
+                HttpURLConnection connection = (HttpURLConnection) new URL(JuliaiApplication.oacs.requestUrl + "/api/generate").openConnection();
                 connection.setRequestMethod("POST");
                 connection.setRequestProperty("Content-Type", "application/json");
                 connection.setDoOutput(true);
 
-                connection.getOutputStream().write(JuliaiApplication.oacs.prepareRequestPayload("llama2",prompt).getBytes());
+                connection.getOutputStream().write(JuliaiApplication.oacs.prepareRequestPayload("llama2", prompt).getBytes());
 
                 Scanner scanner = new Scanner(new InputStreamReader(connection.getInputStream()));
 
-                boolean isFirst = true;
                 while (scanner.hasNextLine()) {
                     String line = scanner.nextLine();
-                    if (line.startsWith("{")) {
-                        JSONObject jsonObject = new JSONObject(line);
-                        data.append(jsonObject.getString("response"));
-                        generate_area.setValue(data.toString());
-                        isFirst = false;
-                    }
-                    System.out.println(line);
+                    JSONObject jsonObject = new JSONObject(line);
+                    data.append(jsonObject.getString("response")); // Accumulate the response in the string builder
+                    generate_area.setValue(data.toString());
+
+                    System.out.println(data.toString());
                 }
-                if (!isFirst) {
-                    System.out.println("-- End of JSON stream --");
-                }
-                generate_area.setReadOnly(false);
-            }catch(Exception e){
-                Notification.show(e.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }
-        else{
+        } else {
             Notification.show("Prompt is empty!");
         }
     }
